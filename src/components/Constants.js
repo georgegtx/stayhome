87bf7186d288675f53e8d1cb59e2a4c8794ec8cf
George Metsis <geoptw@gmail.com>
2020-03-21 00:26:03 +0200

    initial

---
+++ src/components/Constants.js
@@ -0,0 +1,15 @@
+
+export default class Constants {
+  static SUSCEPTIBLE_COLOR = '#EEE';
+  static EXPOSED_COLOR = '#ffdde2';
+  static INFECTED_COLOR = '#F44';
+  static ISOLATING_COLOR = '#8094ff';
+  static REMOVED_COLOR = '#999';
+  static DEAD_COLOR = '#000';
+
+  static SUSCEPTIBLE = 0;
+  static EXPOSED = 1;
+  static INFECTED = 2;
+  static REMOVED = 3;
+  static DEAD = 4;
+}
